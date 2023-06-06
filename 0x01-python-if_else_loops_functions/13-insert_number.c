0a2fc17b836057889ab57aa9bb242cf09206285f
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-06-07 01:51:22 +0200

    Create 13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * create_node - Creates a new node with the given number
+ * @number: The number to be stored in the node
+ *
+ * Return: The address of the new node, or NULL if memory allocation failed
+ */
+
+
+/* Definition of a singly linked list node */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode, *tp;
+
+	newnode = malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		newnode->n = number;
+		newnode->next = *head;
+		*head = newnode;
+        
+		return (newnode);
+	}
+	else 
+        if (number <= (*head)->n)
+	{
+		newnode->n = number;
+		newnode->next = *head;
+        
+		*head = newnode;
+		return (newnode);
+	}
+	else
+	{
+		tp = *head;
+        
+		while (tp->next != NULL && number > tp->next->n)
+		{
+			tp = tp->next;
+		}
+		newnode->n = number;
+		newnode->next = tp->next;
+        
+		tp->next = newnode;
+        
+		return (newnode);
+	}
+	return (NULL);
