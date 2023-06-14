ce4ac5ec344680d20d35471a1c0f5bdb1726ec33
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-06-15 00:58:11 +0200

    Create 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_set = set()
+
+    for num in my_list:
+        
+        unique_set.add(num)
+
+
+    total = sum(unique_set)
+
+    return total
