1a7a177fd90578488c01e5815892147bbcdd71e8
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-06-27 01:03:59 +0200

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for element in my_list:
+            if count >= x:
+                break
+            print(element, end="")
+            count += 1
+        print()
+        return count
+    except:
+        return count
