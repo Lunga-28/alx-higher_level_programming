c3bd12801b607ed0a3c204d0efd044b2bb514a65
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-06-27 01:08:45 +0200

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_function(fct, *args):
+    try:
+        resu = fct(*args)
+        return resu
+    except Exception as e:
+        print("Exception:", str(e), file=sys.stderr)
+        return None
