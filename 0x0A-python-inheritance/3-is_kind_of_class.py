1d4a5d70945e565a4b49198de07179898c4b977f
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-07-11 02:37:26 +0200

    Create 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """
+    returns True if obj is
+    instance of inherited class
+    """
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns True or False
+    """
+    return (isinstance(obj, a_class))
