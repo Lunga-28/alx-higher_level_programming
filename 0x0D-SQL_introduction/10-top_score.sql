38a31bb2d2ae4b3f9f01e802c6d45537752ec347
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-08-17 22:01:55 +0200

    Create 10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- lists all records in order                                                                                                                                           
+SELECT score, name FROM second_table ORDER BY score DESC
