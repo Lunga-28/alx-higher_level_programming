c8308c35bf59b72f31c020f829262ea24228c8d8
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-09-29 20:34:59 +0200

    Create 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+send request to url
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code:', response.status_code)
