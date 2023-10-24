d9d6b039f5f2edabf527327e5c3dd85bcd4a47f2
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-10-24 20:20:56 +0200

    Create 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// writes to a file
+const fs = require('fs');
+const file = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(file, str, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
